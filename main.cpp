9b58d12c84b3642b8c658688624326fc9d56bafe
lbinder <binder.lukas@outlook.com>
2019-12-17 20:44:35 +0100

    added ncurse

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include "AstParse.h"
 #include "AstExecute.h"
 #include <iostream>
+#include <ncurses.h>
+
 int main() {
     AST_Registry reg;
     AST a;
     std::string s;
 
     while(true) {
+        printf("input boolean expression: ");
         getline(std::cin, s);
+        reg.clear();
+        a.data.clear();
         parse(s, &a, &reg);
         TruthTable::print_table(&a, &reg);
+        printf("\n");
     }
     /*parse("a&(b^!c)", &a, &reg);
     TruthTable tt = TruthTable(&a, &reg);
